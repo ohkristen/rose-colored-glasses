856dff8262086c85c6b73e7e4c96cd0b267aba27
Kristen Gunn <ohkristen@yahoo.com>
2016-01-02 13:26:10 -0800

    added functions to open the dialog box to select a folder

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 'use strict';
 
+function openFolderDialog(){
+  var inputField = document.querySelector('#folderSelector')
+  inputField.addEventListener('change', function (){
+    var folderPath = this.value;
+    alert('The folder path is '+ folderPath)
+  },false)
+  inputField.click()
+}
+
 function bindSelectionFolderClick() {
   var button = document.querySelector('#select_folder')
   button.addEventListener('click', function (){
-    alert('clicked on the button')
+    openFolderDialog()
   })
+
 }
 
 // Runs when the browser has loaded the page
