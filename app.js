22d7ef8ebb1fa44fc6206aedc10d2ed6889b33bb
Kristen Gunn <ohkristen@yahoo.com>
2016-01-03 10:21:22 -0800

    working on nwbuild to build the app

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 //Dependencies
-var fs      = require('fs');
-var mime    = require('mime');
-var path    = require('path');
+var fs        = require('fs');
+var mime      = require('mime');
+var path      = require('path');
+var gui       = require('nw.gui');
+
+var photoData = null;
 
 function openFolderDialog(cb){
   var inputField = document.querySelector('#folderSelector')
@@ -83,8 +86,77 @@ function displayPhotoInFullView(){
   document.querySelector('#fullViewPhoto').style.display = 'block'
 }
 
-function backToGridView(){
-  document.querySelector('#fullViewPhoto').style.display = 'none'
+var filters = {
+     original: function () {},
+
+     grayscale: function (item) {
+         item.saturation(-100);
+         item.render();
+     },
+     sepia: function (item) {
+         item.saturation(-100);
+         item.vibrance(100);
+         item.sepia(100);
+         item.render();
+     },
+     sunburst: function (item) {
+         item.brightness(21);
+         item.vibrance(22);
+         item.contrast(11);
+         item.saturation(-18);
+         item.exposure(18);
+         item.sepia(17);
+         item.render();
+     },
+     port: function (item) {
+         item.vibrance(49);
+         item.hue(6);
+         item.gamma(0.6);
+         item.stackBlur(2);
+         item.contrast(11);
+         item.saturation(19);
+         item.exposure(2);
+         item.noise(2);
+         item.render();
+     }
+ };
+
+ function applyFilter (filterName) {
+     Caman('#image', function () {
+         this.reset();
+         filters[filterName](this);
+     });
+ }
+
+ function bindSavingToDisk () {
+    var photoSaver  = document.querySelector('#photoSaver');
+    photoSaver.addEventListener('change', function () {
+        var filePath = this.value;
+        fs.writeFile(filePath, photoData, 'base64', function (err) {
+            if (err) { alert('There was an error saving the photo:',err.message); }
+            photoData = null;
+        });
+    });
+}
+
+function saveToDisk () {
+    var photoSaver  = document.querySelector('#photoSaver');
+    var canvas      = document.querySelector('canvas');
+    photoSaver.setAttribute('nwsaveas','Copy of ' + canvas.attributes['data-name'].value);
+    photoData       = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
+    photoSaver.click();
+}
+
+function backToGridView () {
+    var canvas  = document.querySelector('canvas');
+    if (canvas) {
+        var image   = document.createElement('img');
+        image.setAttribute('id','image');
+        canvas.parentNode.removeChild(canvas);
+        var fullViewPhoto = document.querySelector('#fullViewPhoto');
+        fullViewPhoto.insertBefore(image, fullViewPhoto.firstChild);
+    }
+    document.querySelector('#fullViewPhoto').style.display = 'none';
 }
 
 function bindClickingOnAPhoto(photo){
@@ -93,21 +165,88 @@ function bindClickingOnAPhoto(photo){
   }
 }
 
-// Runs when the browser has loaded the page
-window.onload = function (){
-  bindSelectionFolderClick(function (folderPath){
-    hideSelectFolderButton()
-    findAllFiles(folderPath, function(err, files){
-      if(!err){
-        findImageFiles(files, folderPath, function (imageFiles){
-          imageFiles.forEach(function(file, index){
-            addImageToPhotosArea(file)
-            if(index === imageFiles.length-1){
-              bindClickingOnAllPhotos()
-            }
-          })
-        })
-      }
-    })
-  })
+function clearArea () {
+    document.getElementById('photos').innerHTML = '';
 }
+
+function loadAnotherFolder () {
+    openFolderDialog(function (folderPath) {
+        findAllFiles(folderPath, function (err, files) {
+            if (!err) {
+                clearArea();
+                echo.init({
+                    offset: 0,
+                    throttle: 0,
+                    unload: false
+                });
+                findImageFiles(files, folderPath, function (imageFiles) {
+                    imageFiles.forEach(function (file, index) {
+                        addImageToPhotosArea(file);
+                        if (index === imageFiles.length-1) {
+                            echo.render();
+                            bindClickingOnAllPhotos();
+                            bindSavingToDisk();
+                        }
+                    });
+                });
+            }
+        });
+    });
+}
+
+function loadMenu () {
+    var menuBar     = new gui.Menu({type:'menubar'});
+    var menuItems   = new gui.Menu();
+
+    menuItems.append(new gui.MenuItem({ label: 'Load another folder', click: loadAnotherFolder }));
+
+    var fileMenu = new gui.MenuItem({
+        label: 'File',
+        submenu: menuItems
+    });
+
+    if (process.platform === 'darwin') {
+
+        // Load Mac OS X application menu
+        menuBar.createMacBuiltin('Lens');
+        menuBar.insert(fileMenu, 1);
+
+    } else {
+
+        // Load Windows/Linux application menu
+        menuBar.append(fileMenu, 1);
+
+    }
+
+    gui.Window.get().menu = menuBar;
+
+}
+
+// Runs when the browser has loaded the page
+window.onload = function () {
+
+    echo.init({
+        offset: 0,
+        throttle: 0,
+        unload: false
+    });
+
+    bindSelectFolderClick(function (folderPath) {
+        loadMenu();
+        hideSelectFolderButton();
+        findAllFiles(folderPath, function (err, files) {
+            if (!err) {
+                findImageFiles(files, folderPath, function (imageFiles) {
+                    imageFiles.forEach(function (file, index) {
+                        addImageToPhotosArea(file);
+                        if (index === imageFiles.length-1) {
+                            echo.render();
+                            bindClickingOnAllPhotos();
+                            bindSavingToDisk();
+                        }
+                    });
+                });
+            }
+        });
+    });
+};
